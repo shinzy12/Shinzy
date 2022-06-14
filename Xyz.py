ef1418db551f65b03346578e84f44e802752d13b
shinzy12 <91730481+shinzy12@users.noreply.github.com>
2022-06-15 05:28:10 +0700

    Add files via upload

---
+++ Xyz.py
@@ -0,0 +1,18 @@
+import os
+os.system('termux-setup-storage')
+os.system('git pull')
+try:os.system('mkdir /sdcard/SHIN-DATA')
+except:pass
+try:os.system('mkdir /sdcard/SHIN-DATA/OK')
+except:pass
+try:os.system('mkdir /sdcard/SHIN-DATA/CP')
+except:pass
+try:os.system('mkdir /sdcard/SHIN-DATA/TAP-A2F')
+except:pass
+try:os.system('socks4.txt')
+except:pass
+if __name__ == "__main__":
+        try:
+                __import__("Xyz").login()
+        except Exception as e:
+                exit(str(e))
