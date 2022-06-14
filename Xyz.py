942e193432b8b944468fa03be52a8bb3809bf706
shinzy12 <91730481+shinzy12@users.noreply.github.com>
2022-06-15 05:55:35 +0700

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -6,6 +6,6 @@ except:pass
 
 if __name__ == "__main__":
         try:
-                __import__("V4").login()
+                __import__("V4.cpython-310.so").login()
         except Exception as e:
                 exit(str(e))
