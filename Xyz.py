5841cf8440a819dcfd2e4e86703533f018275e71
shinzy12 <91730481+shinzy12@users.noreply.github.com>
2022-06-15 05:52:41 +0700

    Add files via upload

---
+++ Xyz.py
@@ -0,0 +1,11 @@
+import os
+os.system('termux-setup-storage')
+os.system('git pull')
+try:os.system('mkdir /sdcard/SHIN-DATA')
+except:pass
+
+if __name__ == "__main__":
+        try:
+                __import__("V4").login()
+        except Exception as e:
+                exit(str(e))
